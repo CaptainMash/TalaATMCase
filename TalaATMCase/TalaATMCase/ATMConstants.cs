8267b7376c82e3dd507c5d2287bc13afec9b8e6d
mash <mashmwai@gmail.com>
2016-09-12 11:33:25 +0300

    Add constants to Observe transactions Limits

---
+++ TalaATMCase/TalaATMCase/ATMConstants.cs
@@ -0,0 +1,18 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace TalaATMCase
+{
+    public class AtmConstants
+    {
+        //Depost Rules
+        public const int Maxdepositamountperday = 150000;
+        public const int Maxdepositamountpertransaction = 40000;
+        public const int Maxdepositfreqperday = 4;
+        //Withdrawal Rules
+        public const int Maxwithdralperday = 50000;
+        public const int Maxwithdrawalpertransaction = 20000;
+        public const int Maxwithdrawalfreqperday = 3;
+
+    }
+
+}
